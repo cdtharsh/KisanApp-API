592a78d21698f6f6abc3bbf7beceed1149aeb403
cdtharsh <harshalkenjale2@gmail.com>
2024-08-23 16:44:58 +0530

    Update rateLimit.js

--- security/rateLimit.js
+++ security/rateLimit.js
@@ -9,6 +9,6 @@ export const registrationLimiter = rateLimit({
 
 export const loginLimiter = rateLimit({
     windowMs: 1* 60* 60* 1000, // 1 HRS
-    max: 3, // Limit each IP to 5 registration requests per windowMs
-    message: 'Too many login attempts, please try agian after 24 hours',
+    max: 3, // Limit each IP to 3 registration requests per windowMs
+    message: 'Too many login attempts, please try agian after 1 hour',
 })
