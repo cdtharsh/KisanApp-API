6e0ee0524a27eeb49d76e24ec6ac31143240beaf
cdtharsh <harshalkenjale2@gmail.com>
2024-12-25 00:03:42 +0530

    added rate limits

--- routes/route.js
+++ routes/route.js
@@ -2,24 +2,19 @@ import { Router } from 'express';
 import * as authController from '../controller/authController.js';
 import * as authEmailMiddleware from '../middleware/emailMiddleware.js';
 import * as rateLimit from '../security/rateLimit.js';
-import RateLimit from 'express-rate-limit';
 import * as authMiddleware from '../middleware/authMiddleware.js';
 import * as passController from '../controller/passwordReset.js';
 
 const router = Router();
 
 router.post('/register', rateLimit.registrationLimiter, authController.register);
-const verifyEmailLimiter = RateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // max 100 requests per windowMs
-});
-router.get('/verify-email', verifyEmailLimiter, authEmailMiddleware.verifyEmail);
-router.get('/check-email-verification', authEmailMiddleware.checkEmailVerification);
-router.post('/resend-verification-email', authEmailMiddleware.resendEmailVerification);
-router.post('/forget-password-email', passController.forgotPassword);
-router.post('/reset-with-email', passController.resetPasswordWithOtp);
+router.get('/verify-email', rateLimit.verifyEmailLimiter, authEmailMiddleware.verifyEmail);
+router.get('/check-email-verification', rateLimit.checkEmailVerificationLimiter, authEmailMiddleware.checkEmailVerification);
+router.post('/resend-verification-email', rateLimit.resendEmailVerificationLimiter, authEmailMiddleware.resendEmailVerification);
+router.post('/forget-password-email', rateLimit.forgotPasswordLimiter, passController.forgotPassword);
+router.post('/reset-with-email', rateLimit.resetPasswordWithOtpLimiter, passController.resetPasswordWithOtp);
 
 router.post('/login', rateLimit.loginLimiter, authController.login);
-router.post('/logout',authMiddleware.verifyToken, authController.logout);
+router.post('/logout', authMiddleware.verifyToken, authController.logout);
 
 export default router;
--- security/rateLimit.js
+++ security/rateLimit.js
@@ -12,6 +12,38 @@ export const loginLimiter = rateLimit({
     message: 'Too many login attempts, please try again after 15 minutes',
 });
 
+export const verifyEmailLimiter = RateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 3, // max 20 requests per windowMs
+    message: 'Too many verification attempts, please try again after 15 minutes',
+});
+
+export const checkEmailVerificationLimiter = RateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 20,
+    message: 'Too many verification attempts, please try again after 15 minutes',
+});
+
+export const resendEmailVerificationLimiter = RateLimit(
+    {
+        windowMs: 15 * 60 * 1000,
+        max: 3,
+        message: 'Too many requests, please try again after 15 minutes',
+    }
+);
+
+export const forgotPasswordLimiter = RateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 3,
+    message: 'Too many requests, please try again after 15 minutes',
+});
+
+export const resetPasswordWithOtpLimiter = RateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 3,
+    message: 'Too many requests, please try again after 15 minutes',
+});
+
 export const emailVerificationLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
     max: 10,
